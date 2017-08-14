dbcc3a0c0f1701e785d3bfb4e4e142f1bb9acb3c
Chris Palmer <cpalmer608@gmail.com>
2017-08-13 19:42:54 -0500

    buggy not working

--- public/js/core.js
+++ public/js/core.js
@@ -1,1 +1,20 @@
-angular.module('Todo', ['todoController', 'todoService']);
+(function() {
+    function config($stateProvider, $locationProvider) {
+        $locationProvider
+            .html5Mode({
+                enabled: true,
+                requireBase: false
+            });
+
+        $stateProvider
+            .state('main', {
+                url: '/',
+                controller: 'MainCtrl as main'
+                // templateUrl: '/templates/index.html'
+            })
+    }
+
+    angular
+        .module('Todo', ['ui.router', 'ui.bootstrap'])
+        .config(config);
+})();
--- public/js/services/todos.js
+++ public/js/services/todos.js
@@ -1,14 +1,19 @@
-angular.module('todoService', [])
-	.factory('Todos', ['$http',function($http) {
-		return {
-			get : function() {
-				return $http.get('/api/todos');
-			},
-			create : function(todoData) {
-				return $http.post('/api/todos', todoData);
-			},
-			delete : function(id) {
-				return $http.delete('/api/todos/' + id);
-			}
-		}
-	}]);
+(function() {
+  function Todos($http) {
+    return {
+      get : function() {
+        return $http.get('/api/todos');
+      },
+      create : function(todoData) {
+        return $http.post('/api/todos', todoData);
+      },
+      delete : function(id) {
+        return $http.delete('/api/todos/' + id);
+      }
+    }
+  }
+
+  angular
+    .module('todo')
+    .factory('Todos', ['$http', Todos]);
+})();
